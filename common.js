8f09ea9763dac407ae84a7f42732102c89bccaa8
Ivan Somau <Somovlife@gmail.com>
2017-06-21 23:29:11 +0300

    a few changes. 40 min

--- common.js
+++ common.js
@@ -1,1 +1,12 @@
-const any = () => 'any';
+// const button = document.querySelector('button');
+// button.addEventListener('click', ()=>console.log('ку-ку'));
+
+const getUser = () => {
+    return {
+        user: "Ivan",
+        password: 1234
+    }
+}
+
+let any = getUser();
+console.log(any);
